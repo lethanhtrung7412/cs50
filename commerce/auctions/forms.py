3860eaf0b2bc4e3a06d93c0bf472756f29ef1c19
Trung Le <lethanhtrung7412@gmail.com>
2025-05-15 22:52:31 +0700

    update forms

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from .models import Category
+
+class CategoryForm(forms.Form):
+    category_name = forms.CharField(label="Name")
 
 class ListingForm(forms.Form):
     product_name = forms.CharField(label="Product name", max_length=100)
     img = forms.URLField(label="Image link")
     bid_price = forms.DecimalField(label="Price")
+    category = forms.MultipleChoiceField(
+        choices=Category.objects.get(),
+        label="Category"
+    )
